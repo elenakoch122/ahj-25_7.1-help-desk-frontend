0e6e47b67fd5cea5bd72e644294460c2a7385b23
Elena Kochegarova <lelet@ngs.ru>
2023-02-03 20:19:26 +0700

    layout finished

---
+++ src/js/app.js
@@ -0,0 +1,11 @@
+// const xhr = new XMLHttpRequest();
+
+// xhr.addEventListener('load', () => {
+//   if (xhr.status >= 200 && xhr.status < 300) {
+//     try {
+//       const data = JSON.parse(xhr.responseText);
+//     } catch (e) {
+//       console.error(e);
+//     }
+//   }
+// });
